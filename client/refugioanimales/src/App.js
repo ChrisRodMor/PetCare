27681a793a0b62c74f1585f0bb8548e4fab44d2b
Christian Rodriguez <christian2_21@alu.uabcs.mx>
2024-05-25 22:40:15 -0700

    configuracionProyecto

---
+++ client/refugioanimales/src/App.js
@@ -0,0 +1,13 @@
+import { Button } from "react-bootstrap";
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>hola</h1>
+      <Button>Primary</Button>
+    </div>
+  );
+}
+
+export default App;
