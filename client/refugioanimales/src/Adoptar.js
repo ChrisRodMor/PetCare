d42aa007c026647c7c22950374a9675d9c8f5e7b
Christian Rodriguez <christian2_21@alu.uabcs.mx>
2024-06-08 12:54:27 -0700

    definiendoAdoptar

--- client/refugioanimales/src/Adoptar.js
+++ client/refugioanimales/src/Adoptar.js
@@ -1,10 +1,42 @@
+import { Container, Form } from 'react-bootstrap';
 import Navbarcliente from './Navbarcliente';
 
 function Adoptar(){
     return(
         <div>
             <Navbarcliente/>
-            <h1>Adoptar</h1>
+            <Container>
+                <div className='mt-5 d-flex'>
+
+                    <div className='me-auto'>
+                        <h1 class="h1">Refugio</h1>
+                    </div>
+                    
+                    
+
+                    <div className = "d-flex">
+                        <Form>
+                            <div class="mb-3">
+                                <label for="exampleInputEmail1" class="form-label">Nombre</label>
+                                <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp"></input>
+                            </div>
+                            <div class="mb-3">
+                                <label for="exampleInputPassword1" class="form-label">Password</label>
+                                <input type="password" class="form-control" id="exampleInputPassword1"></input>
+                            </div>
+                            <button type="submit" class="btn btn-primary">Submit</button>
+                        </Form>
+                    </div>
+
+                </div>
+
+                <hr className='my-3' style={{ borderTop: '2px solid #D2D5D8'}}></hr>
+                
+                <div>
+
+                    <h1>segundoDIV</h1>
+                </div>
+            </Container>
         </div>
     );
 }
