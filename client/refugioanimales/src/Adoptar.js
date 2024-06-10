ca61ef6cc2d0b82f5152c8f7a0716ef76e765ddd
Christian Rodriguez <christian2_21@alu.uabcs.mx>
2024-06-09 20:33:40 -0700

    RefugioScreenSINAPI

--- client/refugioanimales/src/Adoptar.js
+++ client/refugioanimales/src/Adoptar.js
@@ -1,40 +1,142 @@
-import { Container, Form } from 'react-bootstrap';
+import React from 'react';
+import { Container, Form, Button, Row, Col } from 'react-bootstrap';
 import Navbarcliente from './Navbarcliente';
+import cuyo from './img/cuyo.jpeg';
+import gato from './img/gato.jpg';
+import perro from './img/perro.jpg';
+import PetCard from './PetCard';
 
-function Adoptar(){
-    return(
+function Adoptar() {
+    return (
         <div>
             <Navbarcliente/>
             <Container>
                 <div className='mt-5 d-flex'>
-
                     <div className='me-auto'>
-                        <h1 class="h1">Refugio</h1>
+                        <h1 className="h1">Refugio</h1>
                     </div>
-                    
-                    
-
-                    <div className = "d-flex">
-                        <Form>
-                            <div class="mb-3">
-                                <label for="exampleInputEmail1" class="form-label">Nombre</label>
-                                <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp"></input>
-                            </div>
-                            <div class="mb-3">
-                                <label for="exampleInputPassword1" class="form-label">Password</label>
-                                <input type="password" class="form-control" id="exampleInputPassword1"></input>
-                            </div>
-                            <button type="submit" class="btn btn-primary">Submit</button>
+                    <div className='d-flex'>
+                        <Form className="d-flex align-items-end">
+                            <Form.Group className="mb-3 me-2" controlId="formNombre">
+                                <Form.Label>Mascota</Form.Label>
+                                <Form.Control type="text" placeholder="Nombre" />
+                            </Form.Group>
+                            <Form.Group className="mb-3 me-2" controlId="formAnimal">
+                                <Form.Label>Tipo de Animal</Form.Label>
+                                <Form.Select>
+                                    <option>Seleccione un tipo</option>
+                                    <option>Perros</option>
+                                    <option>Gatos</option>
+                                    <option>Cuyos</option>
+                                </Form.Select>
+                            </Form.Group>
+                            <Form.Group className="mb-3 me-2" controlId="formRaza">
+                                <Form.Label>Raza</Form.Label>
+                                <Form.Select>
+                                    <option>Seleccione una raza</option>
+                                    <option>Raza 1</option>
+                                    <option>Raza 2</option>
+                                    <option>Raza 3</option>
+                                </Form.Select>
+                            </Form.Group>
+                            <Button variant="primary" type="submit" className="btn btn-warning mb-3">Buscar</Button>
                         </Form>
                     </div>
-
                 </div>
-
                 <hr className='my-3' style={{ borderTop: '2px solid #D2D5D8'}}></hr>
-                
                 <div>
-
-                    <h1>segundoDIV</h1>
+                    <Row>
+                        <Col>
+                            <PetCard
+                                name="Botas"
+                                image={gato}
+                                linkTo="/mascota/botas"
+                            />
+                        </Col>
+                        <Col>
+                            <PetCard
+                                name="Felix"
+                                image={gato}
+                                linkTo="/mascota/Felix"
+                            />
+                        </Col>
+                        <Col>
+                            <PetCard
+                                name="Kai"
+                                image={perro}
+                                linkTo="/mascota/Kai"
+                            />
+                        </Col>
+                    </Row>
+                    <Row>
+                        <Col>
+                            <PetCard
+                                name="Botas"
+                                image={gato}
+                                linkTo="/mascota/botas"
+                            />
+                        </Col>
+                        <Col>
+                            <PetCard
+                                name="Felix"
+                                image={gato}
+                                linkTo="/mascota/Felix"
+                            />
+                        </Col>
+                        <Col>
+                            <PetCard
+                                name="Kai"
+                                image={perro}
+                                linkTo="/mascota/Kai"
+                            />
+                        </Col>
+                    </Row>
+                    <Row>
+                        <Col>
+                            <PetCard
+                                name="Botas"
+                                image={gato}
+                                linkTo="/mascota/botas"
+                            />
+                        </Col>
+                        <Col>
+                            <PetCard
+                                name="Felix"
+                                image={gato}
+                                linkTo="/mascota/Felix"
+                            />
+                        </Col>
+                        <Col>
+                            <PetCard
+                                name="Kai"
+                                image={perro}
+                                linkTo="/mascota/Kai"
+                            />
+                        </Col>
+                    </Row>
+                    <Row>
+                        <Col>
+                            <PetCard
+                                name="Botas"
+                                image={gato}
+                                linkTo="/mascota/botas"
+                            />
+                        </Col>
+                        <Col>
+                            <PetCard
+                                name="Felix"
+                                image={gato}
+                                linkTo="/mascota/Felix"
+                            />
+                        </Col>
+                        <Col>
+                            <PetCard
+                                name="Kai"
+                                image={perro}
+                                linkTo="/mascota/Kai"
+                            />
+                        </Col>
+                    </Row>
                 </div>
             </Container>
         </div>
